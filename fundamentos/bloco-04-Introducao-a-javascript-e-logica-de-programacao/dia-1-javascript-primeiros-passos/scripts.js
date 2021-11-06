74ff9eda3afd1ac9a2d9ec36563868851c79980e
AlanFernandes <josefernandes.alan@gmail.com>
2021-11-06 17:01:53 -0300

    Javascript - Primeiros passos - Variáveis

---
+++ fundamentos/bloco-04-Introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,10 @@
+const myName = 'Alan';
+const birthCity = 'Rio de Janeiro';
+let birthYear = 1986;
+
+birthYear = 2030; // alterando o valor da variavel birthYear
+// birthCity = 'Niteroi'; Não é possível alterar o valor pois a variável birthCity é uma constante.
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
