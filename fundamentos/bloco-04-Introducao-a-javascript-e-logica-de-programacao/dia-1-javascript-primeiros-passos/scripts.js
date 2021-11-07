92cc1a284a6f3cc6510370fe8667ea00742c31ad
AlanFernandes <josefernandes.alan@gmail.com>
2021-11-07 11:47:15 -0300

    exercicio 02

--- fundamentos/bloco-04-Introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-Introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,3 +1,5 @@
+console.log('=== Início exercício 1 ===\n');
+
 const a = 79;
 const b = 86;
 
@@ -14,3 +16,22 @@ console.log(
     + `\nDivisão = ${divisao}`
     + `\nMódulo = ${modulo}`,
 );
+
+console.log('\n=== Fim exercício 1 ===');
+
+console.log('=== Início exercício 2 ===\n');
+
+const num1 = 1789;
+const num2 = 2697;
+
+let maior;
+
+if (num1 > num2) {
+    maior = num1;
+} else {
+    maior = num2;
+}
+
+console.log(maior);
+
+console.log('\n=== Fim exercício 2 ===');
