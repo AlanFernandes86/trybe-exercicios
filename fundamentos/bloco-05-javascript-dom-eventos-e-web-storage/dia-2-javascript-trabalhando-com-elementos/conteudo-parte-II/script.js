4fd14f39b056496b3f644a85bcaa8e98885816ec
AlanFernandes <josefernandes.alan@gmail.com>
2021-11-17 18:26:21 -0300

    Conteúdo - Parte II

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/conteudo-parte-II/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/conteudo-parte-II/script.js
@@ -11,10 +11,27 @@ const terceiroFilho2 = pai.children[2];
 ondeVoceEsta.parentElement.style.color = 'green';
 ondeVoceEsta.firstElementChild.innerText = "Primeiro filho do filho";
 
-console.log(ondeVoceEsta.parentElement);
+
+function createElements() {
+    let irmao = document.createElement('section');
+    irmao.id ='irmao';
+    let filho = document.createElement('section');
+    filho.id = 'filho';
+    let filhoDoPrimeiroFilhoDoFilho = document.createElement('section');
+    filhoDoPrimeiroFilhoDoFilho.id = 'filhoDoPrimeiroFilhoDoFilho';
+    pai.appendChild(irmao);
+    ondeVoceEsta.appendChild(filho);
+    ondeVoceEsta.firstElementChild.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+    console.log(filhoDoPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling)
+}
+
+createElements();
+
+/* console.log(ondeVoceEsta.parentElement);
 console.log(ondeVoceEsta.firstElementChild);
 console.log(primeiroFilho);
 console.log(primeiroFilho2);
 console.log(atencao);
 console.log(terceiroFilho);
-console.log(terceiroFilho2);
+console.log(terceiroFilho2); */
