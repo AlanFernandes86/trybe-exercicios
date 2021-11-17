7cf981256067cce1aa8974eb3676eb1f734648c8
AlanFernandes <josefernandes.alan@gmail.com>
2021-11-17 20:12:13 -0300

    Exercício 02

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -6,3 +6,10 @@ h1.id = 'title';
 h1.innerText = 'Exercício 5.2 - JavaScript DOM';
 
 body.appendChild(h1);
+
+// exercício 02
+const main = document.createElement('main');
+main.className = 'main-content';
+
+body.appendChild(main);
+
