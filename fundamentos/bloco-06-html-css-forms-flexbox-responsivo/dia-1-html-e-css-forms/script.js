e03124ec3f111a04e9c67ef53c5cbfd5f00d05e2
AlanFernandes <josefernandes.alan@gmail.com>
2021-11-29 14:34:43 -0300

    Exercícios incompletos - finalizando 02

--- fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-1-html-e-css-forms/script.js
+++ fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-1-html-e-css-forms/script.js
@@ -1,24 +1,36 @@
 const enviar = document.getElementById('enviar');
 const dataDeInicio = document.getElementById('data-inicio');
+const cpf = document.getElementById('cpf');
 
 enviar.onclick = (event) => {
-  //event.preventDefault();
-  if(validaData(dataDeInicio.value)){
+  event.preventDefault();
+  if (validaData(dataDeInicio.value)) {
     dataDeInicio.setCustomValidity('OMG!');
   }
 };
 
 function validaData(data) {
-  
-
-
-  const dataSplit = data.split('/');  
+  const dataSplit = data.split('/');
   const dia = dataSplit[0];
   const mes = dataSplit[1];
   const ano = dataSplit[2];
 
   return true;
+}
 
+cpf.onkeyup = (event) => {
+  formataCPF(event.target.value);
+};
+
+function formataCPF(value) {
+  const cpfAtual = value;
+  let cpfAtualizado;
+  cpfAtualizado = cpfAtual.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, (regex, argumento1, argumento2, argumento3, argumento4) => {
+     return (argumento1 + '.' + argumento2 + '.' + argumento3 + '-' + argumento4);
+    }
+  );
+  console.log(cpfAtualizado);
+  cpf.value = cpfAtualizado;
 }
 
 /* 
