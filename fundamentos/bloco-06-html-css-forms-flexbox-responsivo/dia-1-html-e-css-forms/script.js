e3db1741b86f1fd89bc8691a7858761dc213378f
AlanFernandes <josefernandes.alan@gmail.com>
2021-11-30 00:30:15 -0300

    Validando CPF

--- fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-1-html-e-css-forms/script.js
+++ fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-1-html-e-css-forms/script.js
@@ -3,17 +3,26 @@ const dataDeInicio = document.getElementById('data-inicio');
 const cpf = document.getElementById('cpf');
 
 enviar.onclick = (event) => {
-  event.preventDefault();
-  if (validaData(dataDeInicio.value)) {
-    dataDeInicio.setCustomValidity('OMG!');
+  //event.preventDefault();
+
+  if (!validaData(dataDeInicio.value)) {
+    dataDeInicio.setCustomValidity('Data inválida!');
   }
 };
 
+
 function validaData(data) {
-  const dataSplit = data.split('/');
-  const dia = dataSplit[0];
-  const mes = dataSplit[1];
-  const ano = dataSplit[2];
+  const dataSplit = data;
+  const regex = /\d+/g
+  const a = dataSplit.match(regex)
+  
+  if (a[0] < 0 || a[0] > 31) {
+    return false;
+  } else if (a[1] < 0 || a[1] > 12 ) {
+    return false;
+  } else if (a[2] < 0) {
+    return false;
+  }
 
   return true;
 }
@@ -25,14 +34,30 @@ cpf.onkeyup = (event) => {
 function formataCPF(value) {
   const cpfAtual = value;
   let cpfAtualizado;
-  cpfAtualizado = cpfAtual.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, (regex, argumento1, argumento2, argumento3, argumento4) => {
+  cpfAtualizado = cpfAtual.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, 
+  function (reg, argumento1, argumento2, argumento3, argumento4) {
+   // console.log(reg);
      return (argumento1 + '.' + argumento2 + '.' + argumento3 + '-' + argumento4);
     }
   );
-  console.log(cpfAtualizado);
+  //console.log(cpfAtualizado);
   cpf.value = cpfAtualizado;
 }
 
+cpf.onblur = (event) => {
+    const cpf = event.target.value;
+    const regex = /\d/g;
+    const result = cpf.match(regex);
+    const regexLetter = /[a-zA-Z]/
+    const result2 = regexLetter.test(cpf)
+    console.log(result2);
+    console.log(result);
+    if(result.length <= 11 && !result2) {
+      
+    } else {
+      alert('CPF inválido');
+    }
+}
 /* 
 Fonte:
 https://stackoverflow.com/questions/10777970/can-i-mark-a-field-invalid-from-javascript 
