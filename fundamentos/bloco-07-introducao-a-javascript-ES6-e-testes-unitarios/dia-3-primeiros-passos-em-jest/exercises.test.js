71d509de312ac8b7528674ab1ee4ec1ebe1ddcaf
AlanFernandes <josefernandes.alan@gmail.com>
2021-12-08 13:40:51 -0300

    Exercicio 01

---
+++ fundamentos/bloco-07-introducao-a-javascript-ES6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercises.test.js
@@ -0,0 +1,27 @@
+const sum = require('./sum');
+const myRemove = require('./my-remove');
+
+describe('Exercício 01:', () => {
+    it('1. sum(4, 5) return 9', () => {
+        expect(sum(4, 5)).toBe(9);
+    });
+    it('2. sum(0, 0) return 0', () => {
+        expect(sum(0, 0)).toBe(0);
+    });
+    it('3. sum(4, "5") throw Error', () => {
+        expect( () => {
+            sum(4, "5")
+        }).toThrow();
+    });
+    it('4. sum(4, "5") throw message "parameters must be numbers"', () => {
+        expect( () => {
+            sum(4, "5")
+        }).toThrowError(/^parameters must be numbers$/);
+    });
+});
+
+describe('Exercício 02:', () => {
+   it('myRemove([1, 2, 3, 4], 3) return [1, 2, 4]', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toStrictEqual([1, 2, 4]);
+   });
+});
