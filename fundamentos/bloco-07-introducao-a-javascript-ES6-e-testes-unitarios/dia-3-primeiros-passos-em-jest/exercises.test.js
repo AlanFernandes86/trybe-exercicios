d777b2f1a1b441cc74c14a373b4c50789e24c7d8
AlanFernandes <josefernandes.alan@gmail.com>
2022-01-03 18:26:32 -0300

    Exercicio 02

--- fundamentos/bloco-07-introducao-a-javascript-ES6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercises.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-ES6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercises.test.js
@@ -9,19 +9,25 @@ describe('Exercício 01:', () => {
         expect(sum(0, 0)).toBe(0);
     });
     it('3. sum(4, "5") throw Error', () => {
-        expect( () => {
+        expect(() => {
             sum(4, "5")
         }).toThrow();
     });
     it('4. sum(4, "5") throw message "parameters must be numbers"', () => {
-        expect( () => {
+        expect(() => {
             sum(4, "5")
         }).toThrowError(/^parameters must be numbers$/);
     });
 });
 
 describe('Exercício 02:', () => {
-   it('myRemove([1, 2, 3, 4], 3) return [1, 2, 4]', () => {
+    it('myRemove([1, 2, 3, 4], 3) returns [1, 2, 4]', () => {
         expect(myRemove([1, 2, 3, 4], 3)).toStrictEqual([1, 2, 4]);
-   });
+    });
+    it('myRemove([1, 2, 3, 4], 3) not returns [1, 2, 3, 4]', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).not.toStrictEqual([1, 2, 3, 4]);
+    });
+    it('myRemove([1, 2, 3, 4], 3) returns [1, 2, 3, 4]', () => {
+        expect(myRemove([1, 2, 3, 4], 5)).toStrictEqual([1, 2, 3, 4]);
+    });
 });
